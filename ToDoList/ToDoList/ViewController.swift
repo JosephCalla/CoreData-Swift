59a33521ef582d60f70317230454e736eaeb1753
Joseph Calla <35270796+JosephCalla@users.noreply.github.com>
2022-11-29 09:15:14 -0500

    Save and Fetch data

--- ToDoList/ToDoList/ViewController.swift
+++ ToDoList/ToDoList/ViewController.swift
@@ -6,14 +6,83 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var table: UITableView!
+    var listTask = [Tarea]()
+    let contexto = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        fetchTask()
     }
 
+    @IBAction func buttonAction(_ sender: Any) {
+        
+        var nameTask = UITextField()
+        let alert = UIAlertController(title: "New", message: "Task", preferredStyle: .alert)
+        let alertAction = UIAlertAction(title: "Add", style: .default) { _ in
+            let newTask = Tarea(context: self.contexto)
+            newTask.name = nameTask.text
+            newTask.done = false
+            self.listTask.append(newTask)
+            self.save()
+        }
+        
+        alert.addTextField { textFieldAlert  in
+            textFieldAlert.placeholder = "Escribe tu texto aquí ..."
+            nameTask = textFieldAlert
+        }
+        
+        alert.addAction(alertAction)
+        present(alert, animated: true)
+    }
+    
+    private func save() {
+        do {
+            try contexto.save()
+        } catch {
+            print(error.localizedDescription)
+        }
+        
+        self.table.reloadData()
+    }
+    
+    func fetchTask() {
+        let request : NSFetchRequest<Tarea> = Tarea.fetchRequest()
+        
+        do {
+            listTask = try contexto.fetch(request)
+        } catch {
+            print(error.localizedDescription)
+        }
+    }
+    
+}
 
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return listTask.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = table.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        let task = listTask[indexPath.row]
+        
+        // operator ternario
+        cell.textLabel?.text = task.name
+        cell.textLabel?.textColor = task.done ? .black : .blue
+        
+        cell.detailTextLabel?.text = task.done ? "Completada": "Por completar"
+        
+        // Marcar con una paloma las tareas completas
+        cell.accessoryType = task.done ? .checkmark : .none
+        return cell
+    }
+    
+    
+    
 }
 
