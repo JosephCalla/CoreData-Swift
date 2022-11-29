5d59bc0bea85b112980b1000a7e19e7015c2b7fe
Joseph Calla <35270796+JosephCalla@users.noreply.github.com>
2022-11-28 23:48:58 -0500

    Setup

---
+++ ToDoList/ToDoList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ToDoList
+//
+//  Created by Joseph Estanislao Calla Moreyra on 28/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
